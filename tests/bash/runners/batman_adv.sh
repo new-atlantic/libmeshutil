6d949d2c31954e4470d6a3b32f809e69c0a8a132
Torsti Schulz <torsti.schulz@iki.fi>
2012-10-15 22:37:22 +0300

    Add test for 'mu_batman_adv_mesh_node_addresses'

--- tests/bash/runners/batman_adv.sh
+++ tests/bash/runners/batman_adv.sh
@@ -3,7 +3,11 @@
 PASSES=0
 FAILURES=0
 
-NETWORK_INTERFACE=$(ifconfig -s -a | head -n 2 | tail -n 1 | awk '{ print $1 }')
+NETWORK_INTERFACE="$1"
+
+if [ -z "$NETWORK_INTERFACE" ]; then
+   NETWORK_INTERFACE=$(ifconfig -s -a | head -n 2 | tail -n 1 | awk '{ print $1 }')
+fi
 
 remove_kmod() {
     modprobe -r batman_adv
@@ -83,10 +87,29 @@ n_bat_nodes() {
     INTERFACE_NAME="bat0"
   fi
 
-  if [ -z "$(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | grep 'No batman nodes in range...')" ]; then
-    echo -n "0"
+  if [ -n "$(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | grep 'No batman nodes in range...')" ]; then
+    echo -n "1"
   else
-    echo -n $(($(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | wc | awk '{ print $1 }') - 2))
+    echo -n $(($(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | wc | awk '{ print $1 }') - 1))
+  fi
+}
+
+bat_node_addresses() {
+  local INTERFACE_NAME="$1"
+
+  if [ -z "$INTERFACE_NAME" ]; then
+    INTERFACE_NAME="bat0"
+  fi
+
+  if [ -z "$(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | egrep -o '^([[:alnum:]][[:alnum:]][:]){5}[[:alnum:]][[:alnum:]]')" ]; then
+    echo -n "NO NODES"
+  else
+    echo -n "$(($(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | wc | awk '{ print $1 }') - 2)) "
+    addresses="$(cat /sys/kernel/debug/batman_adv/$INTERFACE_NAME/originators | egrep -o '^([[:alnum:]][[:alnum:]][:]){5}[[:alnum:]][[:alnum:]]')"
+    for addr in $addresses; do
+      tmp=" $addr$tmp"
+    done
+    echo -n $tmp
   fi
 }
 
@@ -121,7 +144,17 @@ echo ""
 echo -e "\t\t\t\t\t TEST\t\tEXPECTED\t\tRESULT"
 echo -e "\t\t\t\t\t ----\t\t--------\t\t------"
 
-########
+###############################################################################
+
+################################
+##                            ##
+##  Section 1: Kernel module  ##
+##                            ##
+################################
+
+##########################################################
+# 1.1 Check if the batman_adv kernel module is available #
+##########################################################
 
 if [ -f "/lib/modules/$(uname -r)/kernel/net/batman-adv/batman-adv.ko" ]; then
     EXPECTED="TRUE"
@@ -136,50 +169,62 @@ if [ "$EXPECTED" == "FALSE" ]; then
     exit 1
 fi
 
-########
+################################################################################
+# 1.2.a Check that the kernel module is correctly being reported as not loaded #
+################################################################################
 
 remove_kmod
 test_case "Kernel Module Loaded" "kmod_loaded" "FALSE" "\t\t" "\t\t"
 
-####
+############################################################################
+# 1.2.b Check that the kernel module is correctly being reported as loaded #
+############################################################################
 
 insert_kmod
 test_case "Kernel Module Loaded" "kmod_loaded" "TRUE" "\t\t" "\t\t"
 
-########
+############################################
+# 1.3 Test kernel module version retrieval #
+############################################
 
 test_case "Kernel Module Version" "kmod_version" "$(cat /sys/module/batman_adv/version)" "\t\t" "\t"
 
-########
+######################################
+##                                  ##
+##  Section 2: Bat Interface tests  ##
+##                                  ##
+######################################
+
+################################
+# 2.1 Default Interface (bat0) #
+################################
+
+##################################################################################
+# 2.1.1.a Check that interface is reported as not available when kmod not loaded #
+##################################################################################
 
 remove_kmod
 test_case "Default bat interface available" "if_available_default" "FALSE" "" "\t\t"
 
-####
+##############################################################################
+# 2.1.1.b Check that interface is reported as available when it is available #
+##############################################################################
 
 insert_kmod
 add_bat_if
 test_case "Default bat interface available" "if_available_default" "TRUE" "" "\t\t"
 del_bat_if
 
-####
-
-remove_kmod
-test_case "Named bat interface available" "if_available_named" "FALSE" "\t" "\t\t"
-
-####
-
-insert_kmod
-add_bat_if "not_default"
-test_case "Named bat interface available" "if_available_named" "TRUE" "\t" "\t\t"
-del_bat_if "not_default"
-
-########
+#################################################################################
+# 2.1.2.a Check that interface is reported as not being up when kmod not loaded #
+#################################################################################
 
 remove_kmod
 test_case "Default bat interface up" "if_up_default" "FALSE" "\t" "\t\t"
 
-####
+##########################################################################
+# 2.1.2.b Check that interface is reported as being up when interface up #
+##########################################################################
 
 insert_kmod
 add_bat_if
@@ -187,25 +232,16 @@ enable_bat_if
 test_case "Default bat interface up" "if_up_default" "TRUE" "\t" "\t\t"
 del_bat_if
 
-####
-
-remove_kmod
-test_case "Named bat interface up" "if_up_named" "FALSE" "\t" "\t\t"
-
-####
-
-insert_kmod
-add_bat_if "not_default"
-enable_bat_if "not_default"
-test_case "Named bat interface up" "if_up_named" "TRUE" "\t" "\t\t"
-del_bat_if "not_default"
-
-########
+####################################################################################
+# 2.1.3.a Check that an error is reported when asking for hwaddr when no interface #
+####################################################################################
 
 remove_kmod
 test_case "Default bat interface MAC address" "if_hwaddr_default" "ERROR" "" "\t\t"
 
-####
+##################################################################################
+# 2.1.3.a Check that the MAC address is reported when bat interface is available #
+##################################################################################
 
 insert_kmod
 add_bat_if
@@ -213,12 +249,54 @@ BAT_IF_ADDR=$(bat_if_address)
 test_case "Default bat interface MAC address" "if_hwaddr_default" "$BAT_IF_ADDR" "" ""
 del_bat_if
 
-####
+
+#######################
+# 2.2 Named Interface #
+#######################
+
+##################################################################################
+# 2.2.1.a Check that interface is reported as not available when kmod not loaded #
+##################################################################################
+
+remove_kmod
+test_case "Named bat interface available" "if_available_named" "FALSE" "\t" "\t\t"
+
+##############################################################################
+# 2.2.1.b Check that interface is reported as available when it is available #
+##############################################################################
+
+insert_kmod
+add_bat_if "not_default"
+test_case "Named bat interface available" "if_available_named" "TRUE" "\t" "\t\t"
+del_bat_if "not_default"
+
+#################################################################################
+# 2.2.2.a Check that interface is reported as not being up when kmod not loaded #
+#################################################################################
+
+remove_kmod
+test_case "Named bat interface up" "if_up_named" "FALSE" "\t" "\t\t"
+
+##########################################################################
+# 2.1.2.b Check that interface is reported as being up when interface up #
+##########################################################################
+
+insert_kmod
+add_bat_if "not_default"
+enable_bat_if "not_default"
+test_case "Named bat interface up" "if_up_named" "TRUE" "\t" "\t\t"
+del_bat_if "not_default"
+
+####################################################################################
+# 2.2.3.a Check that an error is reported when asking for hwaddr when no interface #
+####################################################################################
 
 remove_kmod
 test_case "Named bat interface MAC address" "if_hwaddr_named" "ERROR" "" "\t\t"
 
-####
+##################################################################################
+# 2.2.3.a Check that the MAC address is reported when bat interface is available #
+##################################################################################
 
 insert_kmod
 add_bat_if "not_default"
@@ -226,37 +304,79 @@ BAT_IF_ADDR=$(bat_if_address "not_default")
 test_case "Named bat interface MAC address" "if_hwaddr_named" "$BAT_IF_ADDR" "" ""
 del_bat_if "not_default"
 
-########
+#######################
+##                   ##
+##  Section 3: Mesh  ##
+##                   ##
+#######################
+
+################################
+# 3.1 Kernel module not loaded #
+################################
 
 remove_kmod
-test_case "Number of nodes (default if)" "n_nodes_if_default" "0" "\t" "\t\t"
 
-####
+##################################
+# 3.1.1 Default interface (bat0) #
+##################################
+
+test_case "Number of nodes (default if)" "n_nodes_if_default" "0" "\t" "\t\t"
+test_case "Node addresses (default if)" "node_addresses_if_default" "ERROR" "\t" "\t\t"
+
+#########################
+# 3.1.2 Named interface #
+#########################
+
+test_case "Number of nodes (named if)" "n_nodes_if_named" "0" "\t" "\t\t"
+test_case "Node addresses (named if)" "node_addresses_if_named" "ERROR" "\t" "\t\t"
+
+############################
+# 3.2 Kernel module loaded #
+############################
+
+##################################
+# 3.2.1 Default interface (bat0) #
+##################################
 
 insert_kmod
+
 add_bat_if
 enable_bat_if
 mount_debug_fs
+
+sleep 60
+
 NNODES=$(n_bat_nodes)
 test_case "Number of nodes (default if)" "n_nodes_if_default" "$NNODES" "\t" "\t\t"
+
+ADDRESSES=$(bat_node_addresses)
+test_case "Node addresses (default if)" "node_addresses_if_default" "$ADDRESSES" "\t" "\t"
+
 del_bat_if
-
-####
-
 remove_kmod
-test_case "Number of nodes (named if)" "n_nodes_if_named" "0" "\t" "\t\t"
 
-####
+#########################
+# 3.2.2 Named interface #
+#########################
 
 insert_kmod
+
 add_bat_if "not_default"
 enable_bat_if "not_default"
 mount_debug_fs
+
+sleep 60
+
 NNODES=$(n_bat_nodes "not_default")
 test_case "Number of nodes (named if)" "n_nodes_if_named" "$NNODES" "\t" "\t\t"
+
+ADDRESSES=$(bat_node_addresses "not_default")
+test_case "Node addresses (named if)" "node_addresses_if_named" "$ADDRESSES" "\t" "\t"
+
 del_bat_if "not_default"
 
-########
+##############################################################################
+
 remove_kmod
 
 echo ""
